48ce6d1d51f5825454ab9b0d8894e5ebb3c153f4
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-10-13 22:48:22 +0530

    added some widgets
    📌Padding : https://api.flutter.dev/flutter/widgets/Padding-class.html
    📌SizedBox : https://api.flutter.dev/flutter/widgets/SizedBox-class.html

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "App title",
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("App title"),
-          backgroundColor: const Color.fromARGB(255, 3, 27, 244),
-        ),
+        backgroundColor: const Color.fromARGB(255, 190, 161, 161),
+        
         body: Center(
-          child: Image.asset(
-            "assets/space1.png",
-            height: 300,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 30,left: 20,right: 10,bottom:30 ),
+                child: Image.asset(
+                  "assets/space1.png",
+                  height: 300,
+                ),
+              ),
+              const SizedBox(
+                width: 0.0,
+                height:5.0,
+                child: Card(child: Text('Hello World!')),
+              ),
+              Image.asset(
+                "assets/space2.png",
+                height: 200,
+              ),
+            ],
           ),
         ),
       ),
