e105929d31d21f6ceb6514b14bc788ff4875ba60
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-10-14 15:11:19 +0530

    added some colors

--- lib/main.dart
+++ lib/main.dart
@@ -11,41 +11,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:SafeArea(
+      home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.black,
           body: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                height: 100,
                 width: double.infinity,
+                height: 100,
                 decoration: BoxDecoration(
-                  color: Colors.amber,borderRadius: BorderRadius.circular(5)
+                  color: const Color.fromARGB(255, 43, 57, 184),
+                  borderRadius: BorderRadius.circular(5),
                 ),
               ),
-              Container(
-                height: 100,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.amber,borderRadius: BorderRadius.circular(5)
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      width: 150,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 4, 184, 64),
+                        borderRadius: BorderRadius.circular(5),
+                      )),
+                  Container(
+                      width: 150,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(5),
+                      ))
+                ],
               ),
               Container(
-                height: 100,
                 width: double.infinity,
+                height: 100,
                 decoration: BoxDecoration(
-                  color: Colors.amber,borderRadius: BorderRadius.circular(5)
+                  color: const Color.fromARGB(255, 255, 0, 0),
+                  borderRadius: BorderRadius.circular(5),
                 ),
               ),
-              Container(
-                height: 200,
-                width: double.infinity, 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 114, 7, 255),
+                        borderRadius: BorderRadius.circular(5),
+                      )),
+                  Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 1, 224, 49),
+                        borderRadius: BorderRadius.circular(5),
+                      )),
+                  Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 150, 19, 19),
+                        borderRadius: BorderRadius.circular(5),
+                      )),
+                ],
+              ),
+             Container(
+                width: double.infinity,
+                height: 100,
                 decoration: BoxDecoration(
-                  color: Colors.amber,borderRadius: BorderRadius.circular(5)
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(5),
                 ),
-              )
+              ),
             ],
+           
           ),
         ),
       ),
